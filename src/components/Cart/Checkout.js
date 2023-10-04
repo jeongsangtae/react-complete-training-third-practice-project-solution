311dffb2bbec6667dd8cd5d0e6839811163b294f
jeongsangtae <wjdtkdxo55@naver.com>
2023-10-05 02:06:57 +0900

    React Complete Training Third Practice Project Solution - form, input handler add (Checkout component edit)

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -2,30 +2,75 @@ import React, { useState } from "react";
 import classes from "./Checkout.module.css";
 
 const Chechout = (props) => {
+  const [nameInput, setNameInput] = useState("");
+  const [streetInput, setStreetInput] = useState("");
+  const [postalInput, setPostalInput] = useState("");
+  const [cityInput, setCityInput] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(nameInput, streetInput, postalInput, cityInput);
+
+    setNameInput("");
+    setStreetInput("");
+    setPostalInput("");
+    setCityInput("");
+  };
+
+  const nameHandler = (event) => {
+    setNameInput(event.target.value);
+  };
+
+  const streetHandler = (event) => {
+    setStreetInput(event.target.value);
+  };
+
+  const postalHandler = (event) => {
+    setPostalInput(event.target.value);
+  };
+
+  const cityHandler = (event) => {
+    setCityInput(event.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor="username">Your Name</label>
-        <input type="text" id="username" />
+        <input
+          type="text"
+          id="username"
+          value={nameInput}
+          onChange={nameHandler}
+        />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input
+          type="text"
+          id="street"
+          value={streetInput}
+          onChange={streetHandler}
+        />
       </div>
       <div className={classes.control}>
         <label htmlFor="post-code">Postal Code</label>
-        <input type="number" id="post-code" />
+        <input
+          type="text"
+          id="post-code"
+          value={postalInput}
+          onChange={postalHandler}
+        />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" value={cityInput} onChange={cityHandler} />
       </div>
 
       <div className={classes.actions}>
         <button onClick={props.onClose}>Cancel</button>
-        <button type="submit" className={classes.submit}>
-          Confirm
-        </button>
+        <button className={classes.submit}>Confirm</button>
       </div>
     </form>
   );
